e35392db409718d4210eea1ea3b42e5d906e0018
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-04-03 13:14:51 +0200

    Add the documentation for all the methods exported

--- index.next.js
+++ index.next.js
@@ -1,15 +1,55 @@
 import React from 'react';
 
 // TODO: replace this object with the the logger API
-const loggerAPI = {
-    log(message) {
-        return console.log(message);
-    },
-};
+const loggerAPI = console;
 
-
+/**
+ * Logger context creation. It's an object containing the `<Provider/>` and the `<Consumer/>`
+ * @type { Consumer, Provider }
+ * @link https://reactjs.org/docs/context.html#api
+ */
 export const Logger = React.createContext(loggerAPI);
+
+/**
+ * Logger context Provider. It can be used to override the default log methods
+ * @type { Provider }
+ * @link https://reactjs.org/docs/context.html#provider
+ * @example
+ *
+ * import { Consumer, Provider } from '@dreipol/react-log'
+ *
+ * <Provider value={console}>
+ *     <Consumer>
+ *          { ({log}) => <App log={log}/> }
+ *     </Consumer>
+ * </Provider>
+ */
 export const Provider = Logger.Provider;
+/**
+ * Logger context consumer
+ * @type { Consumer }
+ * @link https://reactjs.org/docs/context.html#consumer
+ * @example
+ *
+ * import { Consumer } from '@dreipol/react-log'
+ *
+ * <Consumer>
+ *     { ({log}) => <App log={log}/> }
+ * </Consumer>
+ */
 export const Consumer = Logger.Consumer;
 
+
+/**
+ * Consumer alias
+ * @type { Consumer }
+ * @link https://reactjs.org/docs/context.html#consumer
+ * @example
+ *
+ * import Logger from '@dreipol/react-log'
+ *
+ * <Logger>
+ *     { ({log}) => <App log={log}/> }
+ * </Logger>
+ */
 export default Consumer;
